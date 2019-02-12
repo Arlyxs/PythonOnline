51ee6522cc741a195b87a6205ac7ff3a4298523b
Aleix Culture <you@example.com>
2019-02-12 00:31:15 -0400

    added python num_strings file

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ msg = "Hello World"
 print(msg)
 wrd = "My Lord is law"
 print(wrd)
-print(2*5)
-print(10/4)
-print((2+4)/6)
-print(2**4)
+
+#user input
+my_name = input()
+my_name
---
+++ nums_strings.py
@@ -0,0 +1,81 @@
+#user input
+my_name = input()
+my_name
+#2**3 gives 2 to the power three
+#4**.5 give quare root
+
+#stores the value of 5 in a lable called a
+a = 5
+a+a
+#labels in lowercase no spaces and restricted symbols
+my_income = 100
+tax_rate = .01
+my_taxes = my_income*tax_rate
+#Show my taxes
+my_taxes
+    #strings
+#strings use single or double qyuotes to let python remember the sequence of letters
+#strings are stored as a sequene of characters thus can be indexed and searched
+# strings are created using either single or double quotes around characters
+'hello'
+"hello"
+#use combinations of single and double quotes to escape errors
+"Now I'm ready to use single quotes in a string"
+# ## Printing a String
+#correct way to display strings in output is to print them
+print('Hello World 1')
+print('Hello World 2')
+print('Use \n to print a new line')
+print('\n')
+print('See what I mean?')
+
+ #len checks the length of a string
+ #index starts at zero
+len('hello world')
+ #assign a label to a string
+ #label or identifier
+s = 'hello world'
+s
+print(s)
+len(s)
+ #indexing strings (index starts from 0)
+s[0]
+s[4]
+# ###slicing (use a : to grab up to a point)
+#grab everything past first character
+s[1:]
+#grab up to but not including the 3rd index
+s[:3]
+#grab everything
+s[:]
+# ##negative indexing allows us to go backwards
+#one index behind 0 so loops back around to last letter
+s[-1]
+#grab all but the last letter
+s[:-1]
+#grab in step sizes
+s[::1]
+s[::2]
+#print a string backwards
+s[::-1]
+#strings are immutable and cannot be changed eg s[0]='x'
+# ##concatenate strings
+s + ' concatenate me'
+# does not change the value of s
+
+# ##use multiplication to create repitition
+letter = 'z'
+letter*10
+
+# ##Object methods Object.method(paramaters)
+s.upper()
+s.lower()
+s.split()
+#excludes the element it was split on
+s.split('w')
+
+#use the string .format() method to format objects into your strings
+#'string here {var1} then also {var2}'.format(var1='something', var2='somthing2)
+print('This is a string with an {p}'.format(p='insert'))
+print('One: {p}, Two: {p}, Three: {p}'.format(p='Hi!'))
+print('Object 1: {a}, Object 2: {b}, Object 3: {c}'.format(a=1,b='two',c=12.3))
