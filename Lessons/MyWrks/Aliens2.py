1e5bde8845e24ed4a4f9a4e537cf9d311b628b89
Aleix <arlyxs@gmail.com>
2019-02-22 15:55:08 -0400

    altered aliens2 with added code

--- Lessons/MyWrks/Aliens2.py
+++ Lessons/MyWrks/Aliens2.py
@@ -1,4 +1,5 @@
 # user input itn or float or str
+import sys
 x = int(input('enter 1st number '))
 y = int(input('enter 2nd number '))
 z = x + y
@@ -6,18 +7,26 @@ print(z)
 
 # fetch first fharacter on input
 ch = input('enter a characer ')
-print(ch[0]
+print(ch[0])
 
-chr = input('enter a char ')[0]
+chrt = input('enter a char ')[0]
 print(chr)
 
-result= eval(input('enter an expression'))
+result = eval(input('enter an expression'))
 print(result)
 
-import sys
-#index first index [0] is for file name
-#navigate to file in cmd prompt and enter numbers with space between
+# index first index [0] is for file name
+# navigate to file in cmd prompt and enter numbers with space between
 a = int(sys.argv[1])
-b=int(sys.argv[2])
-c=a+b
+b = int(sys.argv[2])
+c = a + b
 print(c)
+
+# for loop with if do not print 5s
+for i in range(1, 21):
+    if i % 5 != 0:
+        print(i)
+
+# print range bckwards
+for i in range(20, 11, -1):
+    print(i)
